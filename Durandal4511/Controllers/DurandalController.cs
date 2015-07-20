d11d56cacb7e197a0cb89e2500dec341efd16f06
BeardedYeti <cmsgoboston41@gmail.com>
2015-07-20 18:57:36 -0400

    Potential template to use for LFG web application - Need to make Chris Bye a contributor - Need his opinion on template for Durandal

---
+++ Durandal4511/Controllers/DurandalController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace Durandal4511.Controllers
+{
+    public class DurandalController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Durandal4511/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Durandal4511.MvcApplication" Language="C#" %>
