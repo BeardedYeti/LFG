ec31c3492ecd798e642d101a100775e3a086e1fb
Colin Smith <lorddragonsbane@gmail.com>
2015-08-07 15:50:04 -0400

    Gulpfile edited but apparently I need a main package.json. Generated package.json via npm init but not sure where it should be located.

--- Web/gulpfile.js
+++ Web/gulpfile.js
@@ -1,6 +1,12 @@
 ﻿var gulp = require('gulp');
 var durandal = require('gulp-durandal');
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('build-portal', function () {
+    durandal({
+        baseDir: 'app',
+        main: 'main.js',
+        output: 'main-built.js',//because we have a main-built.js file
+        almond: true,//almond is running instead of require
+        minify: false
+    }).pipe(gulp.dest('app'));
 });
