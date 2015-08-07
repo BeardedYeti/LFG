237fd0aedce264d79f588295f2d14a060ebddf83
Colin Smith <lorddragonsbane@gmail.com>
2015-08-06 21:22:26 -0400

    Got rid of Weyland, trying to install Gulp-Durandal but need to figure out why Node isn't cooperating

---
+++ Web/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var durandal = require('gulp-durandal');
+
+gulp.task('build-portal', function () {
+    durandal({
+        baseDir: 'app',
+        main: 'main.js',
+        output: 'main-built.js',
+        almond: true,
+        minify: false
+    }).pipe(gulp.dest('app'));
+});
