063fd80b90a3085a792543c835a09047f19012cf
Colin Smith <lorddragonsbane@gmail.com>
2015-08-07 14:51:01 -0400

    Installed Gulp Base, and create gulpfile.js with defaults

--- Web/gulpfile.js
+++ Web/gulpfile.js
@@ -1,12 +1,5 @@
 ﻿var gulp = require('gulp');
-var durandal = require('gulp-durandal');
 
-gulp.task('build-portal', function () {
-    durandal({
-        baseDir: 'app',
-        main: 'main.js',
-        output: 'main-built.js',
-        almond: true,
-        minify: false
-    }).pipe(gulp.dest('app'));
+gulp.task('default', function () {
+    // place code for your default task here
 });
