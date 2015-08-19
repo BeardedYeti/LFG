58125c2eab08aca6626ca82559dff5f2103bc040
Colin Smith <lorddragonsbane@gmail.com>
2015-08-19 19:08:53 -0400

    Browsing games works from the browse menu based off of game titles placed in videoGameList repository file

--- Web/App/viewmodels/browse.js
+++ Web/App/viewmodels/browse.js
@@ -1,18 +1,21 @@
-﻿define(['durandal/app', 'knockout', 'repository/videoGameList'], function (app, ko, videoGameList) {
+﻿define(['plugins/router', 'durandal/app', 'knockout', 'repository/videoGameList'], function (router, app, ko, videoGameList) {
 
     return {
-        displayName: 'Browse Games',        
+        displayName: 'Browse Games',
         features: [
-            'OWIN OAuth support (with 3rd party authentication providers)',
-            'Remember user using local storage',
-            'Secure routing',
-            'Customise views for the user dependent on user roles'
+            'Write up features to be added',
+            'Testy',
+            'Testy',
+            'Testy'
         ],
 
         videoGame: ko.observableArray([]),
         activate: function () {
             this.videoGame(videoGameList.listVideoGames());
         }
-    };
-       
+    }
+        function register() {
+        router.navigate('#/register', 'replace');
+        }
+
 });
